27dc0325a3d6abde7dcc6549402bbb7f1d532088
Pravachan Patra <pravachanpatra@gmail.com>
2025-07-28 15:15:52 -0500

    create training example, partition input sequences and output token

--- RNN.py
+++ RNN.py
@@ -3,14 +3,34 @@ import numpy as np
 
 class RNN:
 
-    def __init__(self):
-        pass
+    def __init__(self, vocabulary, inp_seq_length):
+        self.vocabulary = vocabulary                 # set of all unique tokens
+        self.inp_seq_length = inp_seq_length
 
     # given a token an char or word it returns a one-hot-encoding of that token based on the token to its vocab index and the size of the vocab
     def one_hot_encode(self, token, token_to_indx, vocab_size):
         vector = np.zeros((vocab_size, 1))
         vector[token_to_indx[token]] = 1       # get the index that the token represents in the vocab and set that index-position in the vector equal to ``
+        print(vector.shape)
         return vector
+    
+    def create_training_example(self, names, token_to_indx):
+        X = []  # is an 2d-array, each element is an input of an example, its [1, 2, 3, 4] its the indicies of each token in vocab which is the sequence of input of an example
+        Y = []  # is an 2d-array, each element is an label of an example, its [1] its the index of the toekn in vocab which is the next-token in sequence the label of an example
+
+        # iterate all dinosaur names
+        for cur_name in names:
+            # for every index in current name
+            for i in range(len(cur_name)):  
+                # if ith-index plus the input-sequence-length which is where the output-label-token doesnt exceed the length current-name, then proceed
+                if (i+self.inp_seq_length) < len(cur_name):   # i+self.inp_seq_length is where the target-character is, less-than because we dont want to go to the length of the cur-name just its last index
+                    input_sequence = cur_name[i: i+self.inp_seq_length] # the input-sequence is from the ith-cur-index to the ith plus the inp-seq-length, ending bounds is exclusive so it just gets before the target-character
+                    output_token = cur_name[i+self.inp_seq_length]      # the output-token is the ith-index plus the input-seq-length which gets the index of the token after the inp-seq
+
+                    X.append([ token_to_indx[c] for c in input_sequence ]) # for every character in inp-seq convert it into its index in vocab and put it in the same list
+                    Y.append([ token_to_indx[output_token] ])              # convert the output-character into its index in vocab and put in the same list
+
+        return X, Y     # ith element in X is the input of an example, and the ith elemnet of Y is the ouput-label of that example
 
     def foward():
         pass
@@ -29,7 +49,7 @@ def get_vocabulary_tokens_mappings(text):
     vocab_size = len(vocab)                 # get the size of the vocabulary
     char_to_indx = {ch: i for i, ch in enumerate(vocab)}         # create a mapping from each character in vocab to its index in the vocab
     indx_to_char = {i: ch for i, ch in enumerate(vocab)}         # create a mapping from each index in vocab to that index char in vocab
-    return char_to_indx, indx_to_char, vocab_size
+    return char_to_indx, indx_to_char, vocab_size, vocab
     
 
 def load_dinosaur_names_dataset():
@@ -41,15 +61,18 @@ def load_dinosaur_names_dataset():
 
 
 if __name__ == "__main__":
-    rnn = RNN()
     dinosaur_names = load_dinosaur_names_dataset()
     print(f"Dinosaur names: {dinosaur_names[0:10]}")
 
     # dino-names is a list of examples so combine it into one string, to get unique tokens in this vocab
     # token_to_indx:  maps token to its position in vocab its index
     # indx_to_token:  maps index in vocab to what token corresponds to it
-    token_to_indx, indx_to_token, vocab_size = get_vocabulary_tokens_mappings("".join(dinosaur_names)) 
+    token_to_indx, indx_to_token, vocab_size, vocab = get_vocabulary_tokens_mappings("".join(dinosaur_names)) 
 
+    print("\nThis is our vocabulary: ")
+    print(vocab)
+
+    # PRINT TOKEN MAPPINGS
     print("\nChar->indx dictionary: ")
     count = 0
     for key, value in token_to_indx.items():
@@ -58,7 +81,6 @@ if __name__ == "__main__":
             count += 1
         else:
             break
-    
     print("\nIndx->char dictionary: ")
     count = 0
     for key, value in indx_to_token.items():
@@ -68,5 +90,19 @@ if __name__ == "__main__":
         else:
             break
 
+
+
+
+
+    inp_seq_length=4
+    rnn = RNN(vocabulary=vocab, inp_seq_length=inp_seq_length)
+    
+    # SHOW ONE ENCODING OF TOKEN
     oh_encoding_vector = rnn.one_hot_encode("a", token_to_indx, vocab_size)
-    print(f"One-hot encoding of a token: {oh_encoding_vector[0:5]}")
+    print(f"\nOne-hot encoding of a token: {oh_encoding_vector[0:5]}")
+
+    # CREATE TRAINING EXAMPLES
+    X, Y = rnn.create_training_example(names=dinosaur_names, token_to_indx=token_to_indx)
+    print("\nCREATE TRAINING EXAMPLES")
+    print(X[0:5])
+    print(Y[0:5])
